20579db73287753206ca656f6a5de14876cea5a5
Omaima-Qoo <omayma.qoo@gmail.com>
2022-11-09 05:55:14 -0800

    commit splash and onboarding layouts

---
+++ app/src/main/java/com/ex/score/nine/presentation/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.ex.score.nine.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ex.score.nine.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
