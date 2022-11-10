b2ad1494782540a98057bef9ee5e3e3d30fa16f2
khader <mohammadkhadeer1992@gmail.com>
2022-11-10 15:03:45 +0400

    10/Nov 2022 Thu
    1. Start handle base activity where he have a scores

--- app/src/main/java/com/ex/score/nine/BaseActivity.kt
+++ app/src/main/java/com/ex/score/nine/BaseActivity.kt
@@ -2,8 +2,12 @@ package com.ex.score.nine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import com.ex.score.nine.sharedPreferences.TeamsOrPlayers.getTeamsOrPlayersInSP
+import com.ex.score.nine.sharedPreferences.TeamsOrPlayers.saveTeamsOrPlayersInSP
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -16,5 +20,37 @@ open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
+
+        val teams_cont=findViewById<RelativeLayout>(R.id.teams_cont)
+        val teams_radio=findViewById<RelativeLayout>(R.id.teams_radio)
+        val players_cont=findViewById<RelativeLayout>(R.id.players_cont)
+        val players_radio=findViewById<RelativeLayout>(R.id.players_radio)
+
+        //fill radio button in case not selected yet or selected teams
+        if (!getTeamsOrPlayersInSP(applicationContext).isEmpty()
+            &&
+            (getTeamsOrPlayersInSP(applicationContext).equals("empty")
+                    || getTeamsOrPlayersInSP(applicationContext).equals("teams")))
+        {
+            teams_radio.visibility= View.VISIBLE
+        }else{
+            players_radio.visibility= View.VISIBLE
+        }
+
+
+        //action listener to teams and players
+        teams_cont.setOnClickListener(View.OnClickListener {
+            teams_radio.visibility=View.VISIBLE
+            players_radio.visibility=View.GONE
+            saveTeamsOrPlayersInSP(applicationContext,"teams")
+        })
+
+        players_cont.setOnClickListener(View.OnClickListener {
+            teams_radio.visibility=View.GONE
+            players_radio.visibility=View.VISIBLE
+            saveTeamsOrPlayersInSP(applicationContext,"players")
+        })
+
+
     }
 }
